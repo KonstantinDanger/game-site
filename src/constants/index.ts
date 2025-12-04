53337475c6907ceaaa1224a431820f897fa46ffc
DangerKrip <kostyashka2003@gmail.com>
2025-12-04 10:39:38 +0200

    Fixed validation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,18 +4,17 @@ export const validatePassword = (password: string): string | null => {
   if (!password) {
     return 'Password is required';
   }
-  if (password.length < 8) {
-    return 'Password must contain at least 8 characters';
+  if (password.length < 3) {
+    return 'Password must contain at least 3 characters';
   }
-  if (!/[A-Z]/.test(password)) {
-    return 'Password must contain at least one uppercase letter';
-  }
-  if (!/[a-z]/.test(password)) {
-    return 'Password must contain at least one lowercase letter';
-  }
-  if (!/[0-9]/.test(password)) {
-    return 'Password must contain at least one number';
-  }
+  // if (!/[A-Z]/.test(password)) {
+  //   return 'Password must contain at least one uppercase letter';
+  // }
+  // if (!/[a-z]/.test(password)) {
+  //   return 'Password must contain at least one lowercase letter';
+  // }
+  // if (!/[0-9]/.test(password)) {
+  //   return 'Password must contain at least one number';
+  // }
   return null;
 };
-
